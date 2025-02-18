1922d86ea505c1f2a758acd7385aa51d715a96b9
santhoshT15 <ramsanthoshbms@gmail.com>
2025-02-18 11:12:53 +0530

    admin login changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -89,7 +89,7 @@ const adminLogin = async (req, res) => {
       email === process.env.ADMIN_EMAIL &&
       password === process.env.ADMIN_PASSWORD
     ) {
-      const token = jwt.sign(email + password, process.env.JWT_SECRET, {
+      const token = jwt.sign({ email, password }, process.env.JWT_SECRET, {
         expiresIn: "1h",
       });
       res.json({ success: true, token });
