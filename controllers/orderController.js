b0de538082199292f4528aec03529da545c93c5f
santhoshT15 <ramsanthoshbms@gmail.com>
2025-02-18 11:41:25 +0530

    updated fe live link to orderController.jsx

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Place Order for user fe
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://santhosh-food-deliver-app.netlify.app";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
